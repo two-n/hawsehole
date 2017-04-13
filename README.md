f0095107ed53747ad4cdc2a9faead6e827eb5f5b
Paul Dechov <dechovsky@gmail.com>
2017-04-12 21:36:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Hawsehole ⚓
+
+React component for navigation and transitions among named anchors.
+
+`npm install --save hawsehole` (or `yarn add hawsehole`)
+
+```jsx
+import Hawsehole from 'hawsehole';
+
+<Hawsehole
+  // All props optional. Defaults:
+
+  navComponent="nav"  // tag or React component to wrap nav, or null for no nav
+  hash={false}  // true to enable read and write URL hash
+                // or 'push' to also enable pushState on nav click
+  offset={0}  // offset (in px) from anchor top
+  currentClassName="current"  // class assigned to prominent anchor in viewport,
+                              // and corresponding nav li
+  topClassName="top"  // class also assigned to current anchor if at viewport top
+
+  component="div"  // Container component
+  className={null}  // Passed along to container
+  style={null}  // Passed along to container
+
+>
+  {/* Example: */}
+  <a name="constitution"><h1>The Constitution of the United States</h1></a>
+
+  <a name="preamble"><h2>Preamble</h2></a>
+  <p>We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.</p>
+
+  <section>
+    <a name="article-I"><h2>Article I</h2></a>
+    
+    <section>
+      <a name="article-I-section-1"><h3>Section 1</h3></a>
+      <p>All legislative Powers herein granted shall be vested in a Congress of the United States, which shall consist of a Senate and House of Representatives.</p>
+    </section>
+    
+    <section>
+      <a name="article-I-section-2"><h3>Section 2</h3></a>
+      <p>1: The House of Representatives shall be composed of Members chosen every second Year ...</p>
+    </section>
+
+  </section>
+
+</Hawsehole>
+```
+
+Planned features:
+- scrolling to anchor on demand via prop
+- optionally replacing hash with scrolled anchor, as well as clicked nav option
---
+++ src/index.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import { select, event as d3Event } from 'd3-selection';
+import { transition } from 'd3-transition';
+import { interpolate } from 'd3-interpolate';
+import { bisect } from 'd3-array';
+import { timer } from 'd3-timer';
+
+export default class Hawsehole extends React.PureComponent {
+
+  static defaultProps = {
+    navComponent: 'nav',
+    offset: 0,
+    hash: false,
+    currentClassName: 'current',
+    topClassName: 'top',
+
+    component: 'div',
+    className: null,
+    style: null,
+  }
+
+  state = {
+    current: null,
+    top: false,
+    anchors: [],
+  }
+
+  anchorYOffset(anchor) {
+    const node = select(findDOMNode(this)).select(`a[name='${anchor}']`).node();
+    return node && (window.pageYOffset + node.getBoundingClientRect().top);
+  }
+
+  transitionScrollTo(anchor, offset = 0, push = false) {
+    if (this.props.hash) {
+      if (window.history[push ? 'pushState' : 'replaceState'] != null) {
+        window.history[push ? 'pushState' : 'replaceState']({}, "", `#${anchor}`);
+      } else {
+        window.location[push ? 'assign' : 'replace'](`#${anchor}`);
+      }
+    }
+
+    transition().tween('scroll', () => {
+      const i = interpolate(window.pageYOffset, this.anchorYOffset(anchor) - offset);
+      return t => { window.scrollTo(0, i(t)) };
+    });
+  }
+
+  handleScroll() {
+    const end = window.pageYOffset + findDOMNode(this).getBoundingClientRect().bottom,
+          anchorYOffsets = [ ...this.state.anchors.map(this.anchorYOffset.bind(this)), end ],
+          index = bisect(anchorYOffsets, window.pageYOffset + this.props.offset + 1) - 1,
+          current = this.state.anchors[index],
+          top = Math.abs(window.pageYOffset + this.props.offset - anchorYOffsets[index]) < 1;
+    if (current !== this.state.current || top !== this.state.top) {
+      this.setState({ current, top })
+    }
+  }
+
+  findAnchors() {
+    this.setState({
+      anchors:
+        select(findDOMNode(this))
+          .selectAll('a[name]')
+          .nodes()
+          .map(node => select(node).attr('name'))
+    });
+  }
+
+  componentDidMount() {
+    this.timer = timer(() => {
+      if (this.prevPageYOffset !== window.pageYOffset) this.handleScroll();
+      this.prevPageYOffset = window.pageYOffset;
+    });
+    if (this.props.hash) this.connectHash();
+    this.findAnchors();
+  }
+
+  componentWillUnmount() {
+    this.timer.stop();
+    if (this.props.hash) this.disconnectHash();
+  }
+
+  componentDidUpdate() {
+    select(findDOMNode(this))
+      .selectAll('a[name]')
+      .classed(this.props.currentClassName, false)
+      .classed(this.props.topClassName, false)
+      .filter(`a[name='${this.state.current}']`)
+      .classed(this.props.currentClassName, true)
+      .classed(this.props.topClassName, this.state.top);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.hash && nextProps.hash) this.connectHash();
+    else if (this.props.hash && !nextProps.hash) this.disconnectHash();
+
+    if (this.props.children !== nextProps.children) this.findAnchors();
+  }
+
+  connectHash() {
+    const handleURLChange = () => {
+      const anchor = window.location.hash.slice(1);
+      if (anchor) this.transitionScrollTo(anchor, this.props.offset);
+    };
+
+    handleURLChange();
+    select(window).on(`hashchange.${this.ns = Math.random()}`, () => {
+      handleURLChange();
+      d3Event.preventDefault();
+    });
+    window.history.scrollRestoration = 'manual';
+  }
+  disconnectHash() {
+    select(window).on(`hashchange.${this.ns}`, null);
+    window.history.scrollRestoration = 'auto';
+  }
+
+  render() {
+    const {
+      navComponent: Nav,
+      offset,
+      hash,
+      currentClassName,
+
+      component: Component,
+      style,
+      className,
+      children,
+    } = this.props;
+    
+    const {
+      current,
+      anchors,
+    } = this.state;
+
+    const nav = Nav && (
+      <Nav>
+        <ul>{
+          anchors.map(anchor =>
+            <li key={anchor}>
+              <a
+                href={`#${anchor}`}
+                className={anchor === current ? currentClassName : '' }
+                onClick={event => {
+                  this.transitionScrollTo(anchor, offset, hash === 'push');
+                  event.preventDefault();
+                }}
+              >{anchor}</a>
+            </li>
+          )
+        }</ul>
+      </Nav>
+    );
+
+    return <Component className={className} style={style}>{nav}{children}</Component>;
+  }
+}
